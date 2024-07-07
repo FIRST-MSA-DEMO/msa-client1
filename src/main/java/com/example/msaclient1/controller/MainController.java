7ea2932c3540d7d0b8f03ca56e9f8946f7b390d1
Yoon-Jemin <yjm1800@naver.com>
2024-07-07 23:47:28 +0900

    update msa-client1

--- src/main/java/com/example/msaclient1/controller/MainController.java
+++ src/main/java/com/example/msaclient1/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/ms1/first")
     public String mainP(){
         return "main";
     }
